7879249e1591294ec83ed387ebaaf9381bc0eae0
portfolio_2024 <carinaribeiromoreira@gmail.com>
2024-07-07 16:08:36 +0100

    page home: structured elements + navigation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,10 @@
 exports.__esModule = true;
 var jsx_runtime_1 = require("react/jsx-runtime");
 var Home = function () {
-    return ((0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("h1", { children: "Welcome to Movie Search" }) }));
+  return (0, jsx_runtime_1.jsx)("div", {
+    children: (0, jsx_runtime_1.jsx)("h1", {
+      children: "Welcome to Movie Search",
+    }),
+  });
 };
 exports["default"] = Home;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import "./Home.scss";
+import find from "./../../assets/imgs/find.png";
+import { useNavigate } from "react-router-dom";
 import SearchBar from "../../components/SearchBar/SearchBar.jsx";
-import { fetchMovies } from "../../services/api.js";
-import { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar.jsx";
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
 
-  const handleSubmit = async (term) => {
-    const result = await fetchMovies(term);
-    setMovies(result);
+  /**
+   * Handles the submission of a search term and navigates to the movies page with the search query.
+   *
+   * @param {string} term - The search term to be used as a query parameter.
+   * @returns {void} This function does not return anything.
+   */
+  const handleSearchSubmit = (term) => {
+    navigate(`/movies?query=${term}`);
   };
+
   return (
-    <div>
-      <SearchBar onSubmit={handleSubmit}></SearchBar>
+    <div className="home">
+      <Navbar></Navbar>
+
+      <div className="home__img">
+        <img src={find} alt="find" />
+      </div>
+
+      <SearchBar onSearchSubmit={handleSearchSubmit} />
     </div>
   );
 };
